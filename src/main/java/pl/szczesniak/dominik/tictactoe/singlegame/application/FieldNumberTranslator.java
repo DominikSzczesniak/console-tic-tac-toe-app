a4dc9cb4ee9109763f72dfa5e9d694a22fef6af0
Dominik Szczęśniak <dominikszczesniak128@gmail.com>
2023-03-01 18:59:50 +0100

    BoardPrinter - deleted one of the printing methods as it was redundant

--- src/main/java/pl/szczesniak/dominik/tictactoe/singlegame/application/FieldNumberTranslator.java
+++ src/main/java/pl/szczesniak/dominik/tictactoe/singlegame/application/FieldNumberTranslator.java
@@ -2,8 +2,6 @@ package pl.szczesniak.dominik.tictactoe.singlegame.application;
 
 class FieldNumberTranslator {
 
-
-
     FieldCoordinates toCoordinates(final char letter, final int number, final int length) {
         int numberOfLetter = 0;
         int counter = 0;
