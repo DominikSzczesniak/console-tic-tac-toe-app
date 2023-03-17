618078c18308592d8b8b820d9d0f2ecdc1acdce4
Dominik Szczęśniak <dominikszczesniak128@gmail.com>
2023-03-17 15:39:08 +0100

    main and pom TicTacToe dependency

---
+++ src/main/java/pl/szczesniak/dominik/tictactoe/game/Main.java
@@ -0,0 +1,9 @@
+package pl.szczesniak.dominik.tictactoe.game;
+
+import pl.szczesniak.dominik.tictactoe.game.application.TicTacToeConsoleApp;
+
+public class Main {
+	public static void main(String[] args) {
+		new TicTacToeConsoleApp().run();
+	}
+}
