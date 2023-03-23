adf0db83c8f8996d3eaa1b05f196c312b2dcc67a
Dominik Szczęśniak <dominikszczesniak128@gmail.com>
2023-03-23 23:26:47 +0100

    final version

--- src/main/java/pl/szczesniak/dominik/tictactoe/consoletictactoeapp/Launcher.java
+++ src/main/java/pl/szczesniak/dominik/tictactoe/consoletictactoeapp/Launcher.java
@@ -2,7 +2,7 @@ package pl.szczesniak.dominik.tictactoe.consoletictactoeapp;
 
 import pl.szczesniak.dominik.tictactoe.consoletictactoeapp.game.application.TicTacToeConsoleApp;
 
-public class Main {
+public class Launcher {
 	public static void main(String[] args) {
 		new TicTacToeConsoleApp().run();
 	}
